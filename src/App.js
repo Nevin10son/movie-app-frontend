ec70d3642c658202a0ce6ca55040aefcb6685e56
Nevin10son <nevintenson235@gmail.com>
2024-06-13 05:00:57 +0530

    Search Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddMovies from './components/AddMovies';
+import SearchMovies from './components/SearchMovies';
 
 function App() {
   return (
     <div className="App">
       <AddMovies/>
+      <SearchMovies/>
     </div>
-  );
+  )
 }
 
 export default App;
