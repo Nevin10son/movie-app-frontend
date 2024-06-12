bf413d05cf74379aacd7705026ebaf08c6738bdb
Nevin10son <nevintenson235@gmail.com>
2024-06-13 05:19:29 +0530

    viewall completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddMovies from './components/AddMovies';
 import SearchMovies from './components/SearchMovies';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div className="App">
       <AddMovies/>
       <SearchMovies/>
+      <ViewAll/>
     </div>
   )
 }
